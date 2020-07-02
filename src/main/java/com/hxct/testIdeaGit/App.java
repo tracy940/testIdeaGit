a78e1577cf8cec110c069301b105cf55b2b76e0d
chenggang.dong <dcg5037@126.com>
2020-07-02 14:53:54 +0800

    test

---
+++ src/main/java/com/hxct/testIdeaGit/App.java
@@ -0,0 +1,13 @@
+package com.hxct.testIdeaGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello TestIdeaGit!" );
+    }
+}
